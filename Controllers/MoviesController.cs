13dbdc74df30c520417a25c1f7b053252f8eab18
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-04-28 00:14:42 +0500

    Customer Controller and pages , Part Excersice complete.

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -10,6 +10,22 @@ namespace VidlyV4._1.Controllers
 {
     public class MoviesController : Controller
     {
+        //GET Movies
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
+        }
         // GET: Movies/Random
         public ActionResult Random()
         {
