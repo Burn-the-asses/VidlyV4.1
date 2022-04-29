cfba12b292917e4b1a4e7c8ebcfb334989e807e2
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-04-30 01:29:35 +0500

    till seeding data and populate Memebershiptype

---
+++ Migrations/202204292027587_PopulateMembershipTypes.cs
@@ -0,0 +1,16 @@
+namespace VidlyV4._1.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateMembershipTypes : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
