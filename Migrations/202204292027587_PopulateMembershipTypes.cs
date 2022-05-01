7380ada188904189409fb59bc3ef84948a12c6c3
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-05-02 01:30:29 +0500

    EF lecture practice complete going to start Excercise one.

--- Migrations/202204292027587_PopulateMembershipTypes.cs
+++ Migrations/202204292027587_PopulateMembershipTypes.cs
@@ -7,6 +7,10 @@ namespace VidlyV4._1.Migrations
     {
         public override void Up()
         {
+            Sql("Insert Into Membershiptypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (1,0,0,0)");
+            Sql("Insert Into Membershiptypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (2,30,1,10)");
+            Sql("Insert Into Membershiptypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (3,90,3,15)");
+            Sql("Insert Into Membershiptypes (Id, SignUpFee, DurationInMonths, DiscountRate) VALUES (4,300,12,20)");
         }
         
         public override void Down()
