e29ec5dd1f944e8c386ebd5e4f3394fe56d64254
Burn-the-asses <shahnawazwarriach2323@gmail.com>
2022-05-13 11:38:08 +0500

    Part 4 building form start and Markup Practice done.

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -19,6 +19,11 @@ namespace VidlyV4._1.Controllers
         {
             _context.Dispose();
         }
+
+        public ActionResult New()
+        {
+            return View();
+        }
         public ViewResult Index()
         {
             var customers = _context.Customers.Include(c => c.MembershipType).ToList();
